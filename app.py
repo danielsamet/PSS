1866258e72d73531e959d9bfb1179eda593e866b
Daniel <danielsamet98@gmail.com>
2021-02-08 13:35:42 +0000

    initial web setup
    Signed-off-by: Daniel <danielsamet98@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return redirect(url_for("synthesiser"))
+
+
+@app.route('/synthesiser')
+def synthesiser():
+    return render_template("synthesiser.html")
+
+
+@app.route('/concatenation_setup')
+def concatenation_setup():
+    return render_template("concatenation_setup.html")
+
+
+@app.route('/ml_setup')
+def ml_setup():
+    return render_template("ml_setup.html")
 
 
 if __name__ == '__main__':
