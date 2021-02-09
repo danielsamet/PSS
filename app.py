e71b761ef0084f446238c48452a75237d3ccf572
Daniel <danielsamet98@gmail.com>
2021-02-09 12:26:40 +0000

    updated design and added audio handling
    Signed-off-by: Daniel <danielsamet98@gmail.com>

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+app.add_url_rule("/favicon", "favicon", lambda: redirect(url_for("static", filename="favicon/favicon.ico")))
+app.add_url_rule("/favicon-16x16", "favicon-16x16",
+                 lambda: redirect(url_for("static", filename="favicon/favicon-16x16.png")))
+app.add_url_rule("/favicon-32x32", "favicon-32x32",
+                 lambda: redirect(url_for("static", filename="favicon/favicon-32x32.png")))
+app.add_url_rule("/apple-touch-icon", "apple-touch-icon",
+                 lambda: redirect(url_for("static", filename="favicon/apple-touch-icon.png")))
+app.add_url_rule("/site_webmanifest", "site_webmanifest",
+                 lambda: redirect(url_for("static", filename="favicon/site.webmanifest")))
+
 
 @app.route('/')
 @app.route('/index')
