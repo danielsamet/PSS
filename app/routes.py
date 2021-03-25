7378a8cde8e5be79a5b81009def36aab433dc69e
Daniel <danielsamet98@gmail.com>
2021-03-25 11:54:26 +0000

    added support for recording and saving audio!
    Signed-off-by: Daniel <danielsamet98@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
-from flask import current_app, redirect, url_for, render_template, Blueprint
+import base64
+import os
+
+from flask import current_app, redirect, url_for, render_template, Blueprint, jsonify, request
 
 bp = Blueprint('main', __name__, url_prefix="/")
 
@@ -22,3 +25,13 @@ def concatenation_setup():
 @bp.route('/ml_setup')
 def ml_setup():
     return render_template("ml_setup.html")
+
+
+@bp.route("/save_recording", methods=["POST"])
+def save_recording():
+    with open(os.path.join(current_app.config.get("STATIC_DIR"), "recording", "test.mp3"), "wb") as file:
+        file.write(
+            base64.decodebytes(request.form.get("audio").replace("audio/x-mpeg-3;base64,", "").encode("utf-8"))
+        )
+
+    return jsonify(), 200
--- config.py
+++ config.py
@@ -1,5 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     MIGRATIONS_DIR = "migrations_local"
 
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    STATIC_DIR = os.path.join(basedir, "app", "static")
