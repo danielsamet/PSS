37b524a00c5388970725c2ea259236c606b419b6
Daniel Samet <danielsamet98@gmail.com>
2021-04-29 23:08:23 +0100

    added an extension to install ffmpeg
    Signed-off-by: Daniel Samet <danielsamet98@gmail.com>

--- init_script.py
+++ init_script.py
@@ -1,9 +1,10 @@
-from app import create_app, db
+from app import create_app, db, get_words
 from app.main.models import Phoneme, PhonemeExample
 from app.main.phonemes.phoneme_example_dict import phoneme_words
 
 application = create_app(skip_dir_building=True)
 with application.app_context():
+    application.words = get_words()
     for index, symbol in enumerate(phoneme_words):
         phoneme = Phoneme(symbol, index + 1)
         db.session.add(phoneme)
@@ -11,4 +12,7 @@ with application.app_context():
         for word in phoneme_words[symbol]:
             phoneme.examples.append(PhonemeExample(word, ",".join(application.words[word.upper()])))
 
+        print(symbol, "done")
+
+    print("Complete")
     db.session.commit()
